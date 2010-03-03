1c3fd4e9dca07c6b993f27308bbcf27e2eec1870
Jon Atkinson <jon@jonatkinson.co.uk>
2010-03-03 17:36:17 +0000

    Fixed missing direct_to_template import in urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,6 @@
 from os import path
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
